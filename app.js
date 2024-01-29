b30e6efaba2ed4c9477cc9c0fdb47bbfdf116684
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-01-29 16:35:50 -0500

    looping through URLs in console

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 console.log("Let's get this party started!");
 
 async function getGiphy() {
-const giphies = await axios.get("http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym")
-console.log(giphies)
-console.log(giphies.data.data[0].embed_url)
+  const giphies = await axios.get(
+    "http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+  );
+  console.log(giphies);
+  console.log(giphies.data.data);
+  giphiesData = giphies.data.data;
+  for (let giphyURL of giphiesData) {
+    console.log(giphyURL.embed_url);
+  }
 }
-
