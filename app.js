576f07c9b95863cbf0a9214305f5ded835243d36
Srasulova <rasulova_sabina@yahoo.com>
2024-01-29 16:20:40 -0500

    getting urls from the link

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("Let's get this party started!");
+
+async function getGiphy() {
+const giphies = await axios.get("http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym")
+console.log(giphies)
+console.log(giphies.data.data[0].embed_url)
+}
+
