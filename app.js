737e204633b9c6cfe8b18ba9ee55e4b8affdcbd0
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-01-30 13:02:41 -0500

    added remove btn

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const form = document.querySelector(".searchForm");
 const userInput = document.querySelector("#searchInput");
 const btnSearch = document.querySelector(".btnSearch");
 const giphiesList = document.querySelector(".giphies");
+const btnRemove = document.querySelector(".btnRemove");
 
 async function getGiphy() {
   const randomIdx = Math.floor(Math.random() * 50);
@@ -16,9 +17,14 @@ async function getGiphy() {
   return giphy.data.data[randomIdx].images.original.url;
 }
 
-form.addEventListener("submit", (e) => {
+btnSearch.addEventListener("click", (e) => {
   e.preventDefault();
   displayGiphy();
+});
+
+btnRemove.addEventListener("click", (e) => {
+  e.preventDefault();
+  giphiesList.remove();
 });
 
 async function displayGiphy() {
@@ -29,6 +35,7 @@ async function displayGiphy() {
   const listItem = document.createElement("li");
   listItem.append(img);
   giphiesList.append(listItem);
+  userInput.value = "";
 }
 
 // API key: pjJ8e4H2kim2F7FW6qK7fV9KTiJUO6SI
