d8f2a7e201023c34a688cb48ac5473b8e2f5f697
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-01-29 21:57:26 -0500

    displaying urls on btn click

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 console.log("Let's get this party started!");
 
+const form = document.querySelector(".searchForm");
+const userInput = document.querySelector("#searchInput");
+const btnSearch = document.querySelector(".btnSearch");
+const giphiesList = document.querySelector(".giphies");
+
+//   console.log(giphies);
+//   console.log(giphies.data.data);
+// console.log(giphyURL.embed_url);
+// console.log(giphyURL.url);
+
 async function getGiphy() {
   const giphies = await axios.get(
     "http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
   );
-  console.log(giphies);
-  console.log(giphies.data.data);
   giphiesData = giphies.data.data;
   for (let giphyURL of giphiesData) {
-    console.log(giphyURL.embed_url);
+    console.log(giphyURL.url);
+    const newUrl = document.createElement("li");
+    newUrl.innerText = giphyURL.url;
+    giphiesList.append(newUrl);
   }
 }
+
+// console.log(getGiphy());
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getGiphy();
+});
