39f01a9c72dc476dede8b80cb3adf28f474b03e4
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-01-30 12:47:18 -0500

    displaying gifs

--- app.js
+++ app.js
@@ -5,25 +5,30 @@ const userInput = document.querySelector("#searchInput");
 const btnSearch = document.querySelector(".btnSearch");
 const giphiesList = document.querySelector(".giphies");
 
-//   console.log(giphies);
-//   console.log(giphies.data.data);
-// console.log(giphyURL.embed_url);
-// console.log(giphyURL.url);
-
 async function getGiphy() {
   const randomIdx = Math.floor(Math.random() * 50);
   const giphy = await axios.get(
-    `http://api.giphy.com/v1/gifs/search?q=${userInput.value}&api_key=pjJ8e4H2kim2F7FW6qK7fV9KTiJUO6SI`
+    `https://api.giphy.com/v1/gifs/search?q=${userInput.value}&api_key=pjJ8e4H2kim2F7FW6qK7fV9KTiJUO6SI`
   );
   console.log(giphy);
   console.log(randomIdx);
-  console.log(giphy.data.data[randomIdx].url);
-  return giphy.data.data[randomIdx].url;
+  // console.log(giphy.data.data[randomIdx].url);
+  return giphy.data.data[randomIdx].images.original.url;
 }
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  getGiphy();
+  displayGiphy();
 });
+
+async function displayGiphy() {
+  const img = document.createElement("img");
+  img.src = await getGiphy();
+  img.alt = "giphy";
+  console.log(img);
+  const listItem = document.createElement("li");
+  listItem.append(img);
+  giphiesList.append(listItem);
+}
 
 // API key: pjJ8e4H2kim2F7FW6qK7fV9KTiJUO6SI
